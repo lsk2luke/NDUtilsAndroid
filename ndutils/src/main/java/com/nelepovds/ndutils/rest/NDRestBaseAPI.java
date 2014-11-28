5fa36f6368c01f7b566887eccf9596a275eb6a6c
Dmitry Nelepov <nelepovds@gmail.com>
2014-11-28 17:07:52 +0300

    RestAPI base class:

---
+++ ndutils/src/main/java/com/nelepovds/ndutils/rest/NDRestBaseAPI.java
@@ -0,0 +1,51 @@
+package com.nelepovds.ndutils.rest;
+
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+import retrofit.converter.GsonConverter;
+
+/**
+ * Created by Administrator on 28.11.14.
+ */
+public abstract class NDRestBaseAPI<T> {
+
+
+    protected final RestAdapter adapter;
+    public final T service;
+
+    protected abstract String getWatcherID();
+
+
+    public NDRestBaseAPI(String endPoint, Class<T>  restInterfaceClass){
+        RequestInterceptor requestInterceptor = new RequestInterceptor() {
+            @Override
+            public void intercept(RequestFacade request) {
+                if (getWatcherID() != null) {
+                    request.addQueryParam("WatcherID", getWatcherID());
+                    request.addQueryParam("AppOS", "Android");
+                }
+            }
+        };
+
+        this.adapter = new RestAdapter.Builder()
+                .setEndpoint(endPoint)
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setLog(new RestAdapter.Log() {
+                    public void log(String msg) {
+                        Log.i("retrofit", msg);
+                    }
+                })
+                .setRequestInterceptor(requestInterceptor)
+                .setConverter(new GsonConverter(BaseClass.gsonAdapter()))
+
+                .setClient(new OkClient(new OkHttpClient()))
+                .build();
+        this.service = this.adapter.create(restInterfaceClass);
+    }
+
+}
